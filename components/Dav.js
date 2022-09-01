541e3d11eae6be6e48a08bf7a2919372e574215f
dvdapogeo <dvdingles@hotmail.com>
2022-09-01 08:43:45 +0200

    bajo resolucion textura

--- components/Dav.js
+++ components/Dav.js
@@ -6,7 +6,7 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Dav(props) {
-  const { nodes, materials } = useGLTF("/3D BAURFE---para despliegue.gltf");
+  const { nodes, materials } = useGLTF("/5D BAURFE---para despliegue.gltf");
   return (
     <group {...props} dispose={null}>
       <group position={[0, 0, -0.01]}>
@@ -33,4 +33,4 @@ export function Dav(props) {
   );
 }
 
-useGLTF.preload("/3D BAURFE---para despliegue.gltf");
+useGLTF.preload("/5D BAURFE---para despliegue.gltf");
